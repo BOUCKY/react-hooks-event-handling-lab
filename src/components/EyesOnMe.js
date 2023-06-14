0fdc3343dd36749d43dbc744bc9553f59b5ecf97
Alexis Boucouvalas <alexisbou16@gmail.com>
2023-06-14 14:45:07 -0400

    Complete Lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    return(
+        <div>
+            <button
+                onFocus={() => console.log("Good!")}
+                onBlur={() => console.log("Hey! Eyes on me!")}
+            >
+                Eyes on Me
+            </button>
+        </div>
+    )
+}
+
+export default EyesOnMe
